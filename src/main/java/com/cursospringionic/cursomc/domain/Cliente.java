26d69fb2fde288665042358b12eef8b21ccc81c4
estevamponte <tecoponte@gmail.com>
2020-06-07 14:34:06 -0300

    Pedido, Pagamento, EstadoPagamento

--- src/main/java/com/cursospringionic/cursomc/domain/Cliente.java
+++ src/main/java/com/cursospringionic/cursomc/domain/Cliente.java
@@ -18,7 +18,7 @@ import com.cursospringionic.cursomc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-public class Cliente implements Serializable{
+public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -36,6 +36,9 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefone = new HashSet<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente () {
 		
@@ -105,6 +108,14 @@ public class Cliente implements Serializable{
 	public void setTelefone(Set<String> telefone) {
 		this.telefone = telefone;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -130,6 +141,8 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	
