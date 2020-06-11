7f1fa042309c153134b88fcf6a36987786b6666d
estevamponte <tecoponte@gmail.com>
2020-06-11 14:32:46 -0300

    Deletando uma Categoria com Delete

---
+++ src/main/java/com/cursospringionic/cursomc/services/exeptions/DataIntegrityException.java
@@ -0,0 +1,13 @@
+package com.cursospringionic.cursomc.services.exeptions;
+
+public class DataIntegrityException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DataIntegrityException(String msg) {
+		super(msg);
+	}
+	
+	public DataIntegrityException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
