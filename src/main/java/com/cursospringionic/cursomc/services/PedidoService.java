c03b7745e9132984367a95649ddc5bbbdbdc91bf
estevamponte <tecoponte@gmail.com>
2020-06-13 18:54:12 -0300

    Inserindo Pedido

--- src/main/java/com/cursospringionic/cursomc/services/PedidoService.java
+++ src/main/java/com/cursospringionic/cursomc/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.cursospringionic.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cursospringionic.cursomc.domain.ItemPedido;
+import com.cursospringionic.cursomc.domain.PagamentoComBoleto;
 import com.cursospringionic.cursomc.domain.Pedido;
+import com.cursospringionic.cursomc.domain.enums.EstadoPagamento;
+import com.cursospringionic.cursomc.repositories.ItemPedidoRepository;
+import com.cursospringionic.cursomc.repositories.PagamentoRepository;
 import com.cursospringionic.cursomc.repositories.PedidoRepository;
 import com.cursospringionic.cursomc.services.exeptions.ObjectNotFoundException;
 
@@ -15,10 +22,45 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoSevice;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencerPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoSevice.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}
 }
